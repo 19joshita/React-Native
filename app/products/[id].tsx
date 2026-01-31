3c28c0a5ed269c0af432a349b8a113a6d9dff8ad
Bhavishya-root <pateljoshita236@gmail.com>
2026-01-31 16:27:42 +0530

    changes

--- app/products/[id].tsx
+++ app/products/[id].tsx
@@ -1,7 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
 
 const ProductDetail = () => {
+  const getAsyncData = () => {
+    const storage = AsyncStorage.getItem("cart");
+  };
   return (
     <View>
       <Text>ProductDetail</Text>
